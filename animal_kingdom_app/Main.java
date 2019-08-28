cdc9c6cbc1c3cab7c94c45c256b305fad05fa9a3
Koua Xiong <kxiong517@gmail.com>
2019-08-28 14:42:13 -0500

    created a list containing all the animals

--- animal_kingdom_app/Main.java
+++ animal_kingdom_app/Main.java
@@ -1,10 +1,11 @@
 package animal_kingdom_app;
 
+import java.util.*;
+
 public class Main
 {
-    public static void createAnimals() 
+    public static List mammals() 
     {
-        // Mammals
         Mammal panda = new Mammal("Panda", 1869);
         Mammal zebra = new Mammal("Zebra", 1778);
         Mammal koala = new Mammal("Koala", 1816);
@@ -13,23 +14,61 @@ public class Main
         Mammal raccoon = new Mammal("Raccoon", 1758);
         Mammal bigFoot = new Mammal("Big Foot", 2021);
 
-        // Birds
+        List<Mammal> allMammals = new ArrayList<Mammal>();
+
+        allMammals.add(panda);
+        allMammals.add(zebra);
+        allMammals.add(koala);
+        allMammals.add(sloth);
+        allMammals.add(armadillo);
+        allMammals.add(raccoon);
+        allMammals.add(bigFoot);
+
+        return allMammals;
+    }
+
+    public static List birds()
+    {
         Bird pigeon = new Bird("Pigeon", 1837);
         Bird peacock = new Bird("Peacock", 1821);
         Bird toucan = new Bird("Toucan", 1758);
         Bird parrot = new Bird("Parrot", 1824);
         Bird swan = new Bird("Swan", 1758);
 
-        // Fish
+        List<Bird> allBirds = new ArrayList<Bird>();
+
+        allBirds.add(pigeon);
+        allBirds.add(peacock);
+        allBirds.add(toucan);
+        allBirds.add(parrot);
+        allBirds.add(swan);
+
+        return allBirds;
+    }
+
+    public static List fishes()
+    {
         Fish salmon = new Fish("Salmon", 1758);
         Fish catfish = new Fish("Catfish", 1817);
         Fish perch = new Fish("Perch", 1758);
 
-        System.out.println(panda.showState());
+        List<Fish> allFishes = new ArrayList<Fish>();
+
+        allFishes.add(salmon);
+        allFishes.add(catfish);
+        allFishes.add(perch);
+
+        return allFishes;
     }
 
     public static void main(String[] args)
     {
-        createAnimals();
+        List<AbstractAnimal> animals = new ArrayList<AbstractAnimal>();
+
+        animals.addAll(mammals());
+        animals.addAll(birds());
+        animals.addAll(fishes());
+
+        animals.forEach((a)->System.out.println(a.showState()));
     }
 }
--- animal_kingdom_app/Mammal.java
+++ animal_kingdom_app/Mammal.java
@@ -2,7 +2,7 @@ package animal_kingdom_app;
 
 public class Mammal extends AbstractAnimal
 {
-    // Mammals move - walk, breath - lungs, reproduce - live births
+
     public Mammal(String name, int yearDiscovered)
     {
         super(name, yearDiscovered);
